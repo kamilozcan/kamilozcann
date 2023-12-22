2067fc10d027a85137f9198a9643ca3dccce71f9
Kamil Ozcan <kamilozcann@gmail.com>
2023-12-22 14:20:09 -0500

    Modified CoffeApp page

--- src/components/Works/CoffeeApp/CoffeeApp.js
+++ src/components/Works/CoffeeApp/CoffeeApp.js
@@ -2,12 +2,13 @@ import React from "react";
 
 import "./coffeeapp.css";
 
-
-
 const CoffeeApp = () => {
   return (
     <section id="coffeeApp">
       <h2 className="worksTitle">Coffee App</h2>
+      <span>
+        <p style={{ color: "red" }}>(This project is in progress)</p>
+      </span>
       <span className="source">
         <a
           href="https://github.com/kamilozcan/Coffee-App-React-Native"
@@ -24,11 +25,55 @@ const CoffeeApp = () => {
       </span>
       <div className="portfolioImgs">
         <img
-          src={require("../../../assets/Coffees/coffee.all.PNG")}
+          src="/assets/Coffees/coffee.all.PNG"
           alt="coffeeAll"
           className="portfolioImg"
         />
-
+        <img
+          src="/assets/Coffees/coffee.cappuccino.PNG"
+          alt="coffeeAll"
+          className="portfolioImg"
+        />
+        <img
+          src="/assets/Coffees/coffee.espresso.PNG"
+          alt="coffeeAll"
+          className="portfolioImg"
+        />
+        <img
+          src="/assets/Coffees/coffee.cappuccino1.PNG"
+          alt="coffeeAll"
+          className="portfolioImg"
+        />
+        <img
+          src="/assets/Coffees/coffee.espresso1.PNG"
+          alt="coffeeAll"
+          className="portfolioImg"
+        />
+        <img
+          src="/assets/Coffees/coffee.latte.PNG"
+          alt="coffeeAll"
+          className="portfolioImg"
+        />
+        <img
+          src="/assets/Coffees/coffee.flatwhite.PNG"
+          alt="coffeeAll"
+          className="portfolioImg"
+        />
+        <img
+          src="/assets/Coffees/cart.PNG"
+          alt="coffeeAll"
+          className="portfolioImg"
+        />
+        <img
+          src="/assets/Coffees/favorite.PNG"
+          alt="coffeeAll"
+          className="portfolioImg"
+        />
+        <img
+          src="/assets/Coffees/orderhistory.PNG"
+          alt="coffeeAll"
+          className="portfolioImg"
+        />
       </div>
     </section>
   );
